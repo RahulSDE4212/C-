eec77182af1460640724c49d2eceb3ebeaef607f
RahulSDE4212 <160782647+RahulSDE4212@users.noreply.github.com>
2025-11-22 21:56:59 +0530

    namespace practiced

---
+++ CSharpPractice/CSharpPractice/Namespace/namespace.cs
@@ -0,0 +1,11 @@
+namespace CSharpPractice.Namespace
+{
+    public class NamespaceExample
+    {
+        public string GetMessage()
+        {
+            Console.WriteLine("Message from NamespaceExample class.");
+            return "Hello from CSharpPractice.Namespace!";
+        }
+    }
+}
--- CSharpPractice/CSharpPractice/Program.cs
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using CSharpPractice.Namespace;
+
+NamespaceExample ne = new NamespaceExample();
+ne.GetMessage();
 Console.WriteLine("Hello, World!");
